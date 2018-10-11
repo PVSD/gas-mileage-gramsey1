7a188b98056d091fd1e69bfdcec65a2fba7a30ae
gramsey1 <graceramsey312@yahoo.com>
2018-10-11 19:51:03 -0400

    aye

--- src/com/company/Automobile.java
+++ src/com/company/Automobile.java
@@ -4,4 +4,25 @@ package com.company;
  * Created by dpennebacker on 10/9/16.
  */
 public class Automobile {
+    public int currentGallons = 0;
+    public int miles = 0;
+    public int MPG = 0;
+    public Automobile(int milesPerGallon){
+        MPG = milesPerGallon;
+    }
+
+    public void fillUp(int gallons){
+        currentGallons += gallons;
+    }
+    public void takeTrip(int miles){
+        this.miles = miles;
+
+        currentGallons = currentGallons - (miles/MPG);
+    }
+    public double reportFuel(){
+        double fuel = currentGallons;
+
+        return fuel;
+    }
+
 }
